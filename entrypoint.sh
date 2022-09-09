1081578ab97deb6edfa5f99a6454914ec594b227
RC <rc@MacBook-Air-de-RC.local>
2022-09-09 08:52:11 -0300

    Python

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/sh -l
-echo "pushgatey_url: $1 job: $2 metric_name: $3 metric_description: $4 metric_labels: $5"
-python /pushgateway.py --pushgatey_url $1 --job $2 --metric_name $3 --metric_description $4 --metric_labels $5
+python /pushgateway.py --pushgatey_url "$1" --job "$2" --metric_name "$3" --metric_description "$4" --metric_labels "$5"
